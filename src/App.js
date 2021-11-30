a39510c0b63a6d48eddce7d46bda0c953e79c67f
Minji <321963741@naver.com>
2021-12-01 07:56:32 +0900

    facebook-message start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import {useEffect, useState} from 'react';
-import Router from './components/Router';
-import {authService} from './fbase';
-
 function App() {
-  const [init, setInit] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    authService.onAuthStateChanged((user) => {
-      if (user) {
-        setLoggedIn(true);
-      } else {
-        setLoggedIn(false);
-      }
-      setInit(true);
-    });
-  });
-
-  return <>{init ? <Router loggedIn={loggedIn} /> : 'initializing . . .'}</>;
+  return <h1>home</h1>;
 }
 
 export default App;
